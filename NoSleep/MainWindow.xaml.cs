5bbac7c7e9766a5f854d0a06160eb639bf6f73a1
jgarcia <jgarcia@NBR0020>
2021-09-09 15:32:01 -0300

    Add project files.

---
+++ NoSleep/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NoSleep
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private bool _status;
+        private bool Status { get => _status;
+            set
+            {
+                _status = value;
+                if (_status)
+                {
+                    textStatus.Text = "Sleep is disabled";
+                    textStatus.Foreground = Brushes.Red;
+                    NoSleepCore.PreventSleep();
+                }
+                else
+                {
+                    textStatus.Text = "Sleep is enabled";
+                    textStatus.Foreground = Brushes.Green;
+                    NoSleepCore.UndoPreventSleep();
+                }
+            }
+        }
+        public MainWindow()
+        {
+            InitializeComponent();
+            Status = false;
+        }
+        public void HandleCheck(object sender, RoutedEventArgs e)
+        {
+            Status = true;
+        }
+        public void HandleUncheck(object sender, RoutedEventArgs e)
+        {
+            Status = false;
+        }
+    }
+}
---
+++ NoSleep/NoSleepCore.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace NoSleep
+{
+    public class NoSleepCore
+    {
+        enum EXECUTION_STATE : uint
+        {
+            ES_AWAYMODE_REQUIRED = 0x00000040,
+            ES_CONTINUOUS = 0x80000000,
+            ES_DISPLAY_REQUIRED = 0x00000002,
+            ES_SYSTEM_REQUIRED = 0x00000001,
+            INVALID = 0
+        }
+        static EXECUTION_STATE previousState;
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        static extern EXECUTION_STATE SetThreadExecutionState(EXECUTION_STATE esFlags);
+        
+        public static void PreventSleep()
+        {
+            previousState = SetThreadExecutionState(EXECUTION_STATE.ES_DISPLAY_REQUIRED | EXECUTION_STATE.ES_CONTINUOUS);
+        }
+        public static void UndoPreventSleep()
+        {
+            if (previousState != EXECUTION_STATE.INVALID)
+            {
+                SetThreadExecutionState(previousState | EXECUTION_STATE.ES_CONTINUOUS);
+            }
+            
+        }
+
+       
+    }
+}
